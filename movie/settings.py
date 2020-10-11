82c13e7e3d9a98b75d4fa0669acf96bcf8caaa76
jpwarren610 <69476428+jpwarren610@users.noreply.github.com>
2020-10-11 18:19:20 -0500

    Edited timeout

--- movie/settings.py
+++ movie/settings.py
@@ -142,7 +142,7 @@ JWT_AUTH = {
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
     'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=900),
     'JWT_ALLOW_REFRESH': True,
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
 }
